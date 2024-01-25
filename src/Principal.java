a655b4eac4430a6b162a0725158555efc4e8386b
Felipe Braga Duarte <134644788+obragaa-isensi@users.noreply.github.com>
2024-01-24 21:19:19 -0300

    Imprimindo atributos do meu Filme no serial.

---
+++ src/Principal.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "Até o último homem";
+        meuFilme.anoDeLancamento = 2017;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println("Nome do filme: " + meuFilme.nome +
+                ", Ano de lançamento: " + meuFilme.anoDeLancamento +
+                ", Duração em minutos: " + meuFilme.duracaoEmMinutos);
+    }
+}
